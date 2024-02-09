068865552b9cf488a278eca03f30a3ba9bb16a42
amauryrlm <92987817+amauryrlm@users.noreply.github.com>
2024-02-09 15:57:21 +0100

    solve generalise working !!!!!

--- EST3D.py
+++ EST3D.py
@@ -29,8 +29,8 @@ class EST3D:
     
         index_connection1 = self.est1.connection_index
         index_connection2 = self.est2.connection_index 
-        self.est1.plot_steiner_tree()
-        self.est2.plot_steiner_tree()
+        # self.est1.plot_steiner_tree()
+        # self.est2.plot_steiner_tree()
 
 
         lens1 = len(self.est1.terminals) - 2
@@ -99,124 +99,100 @@ class EST3D:
         last_connection = np.array([[index_connection_s1[-1,1],index_connection_s2[-1,1]]])
 
         #concatenate all the connexions
-        print(f'index_connection1 : {index_connection1}')
-        print(f'index_connection_s1 : {index_connection_s1}')
-        print(f'index_connection2 : {index_connection2}')
-        print(f'index_connection_s2 : {index_connection_s2}')
-        print(f'last_connection : {last_connection}')
         list_index = np.concatenate((index_connection1,index_connection2,index_connection_s1,index_connection_s2,last_connection),axis=0)
-        print(f'list_index : {list_index}')
         self.connection_index = list_index
 
+        #create the connection matrix
         matrix_index = np.zeros((lent1 + lent2 + lens1 + lens2 + 2,lent1 + lent2 + lens1 + lens2 + 2))
-
         for i in range(len(list_index)):
             matrix_index[int(list_index[i][0]) ,int( list_index[i][1])] = 1
-        
         self.connection_matrix = matrix_index
-        print(f'connection_matrix : \n {self.connection_matrix}')
+
+        #the index that corresponds to the steiner points
+        s_index = set()
+        for i in range(len(index_connection_s1)):
+            s_index.add(index_connection_s1[i][0])
+            s_index.add(index_connection_s1[i][1])
+
+        for i in range(len(index_connection_s2)):
+            s_index.add(index_connection_s2[i][0])
+            s_index.add(index_connection_s2[i][1])
+
+        #the index that corresponds to the terminals
+        t_index = set()
+        for i in range(len(list_index)):
+            t_index.add(list_index[i][0])
+            t_index.add(list_index[i][1])
+
+        t_index = t_index - s_index
+
+
+        t_index = np.array(list(t_index))
+        s_index = np.array(list(s_index))
+        t_index = np.sort(t_index)
+        s_index = np.sort(s_index)
+
+
 
         self.terminals = np.zeros((self.num_t,3))
+
+        
+
+        #make a dictionary that contains the index of the terminals and their coordinates
 
         for i in range(len(self.est1.terminals)):
             self.terminals[i] = np.array([self.est1.terminals[i][0],self.est1.terminals[i][1],self.z0])
         for i in range(len(self.est1.terminals),len(self.est1.terminals) + len(self.est2.terminals)):
-            print(f'i : {i}')
             self.terminals[i] = np.array([self.est2.terminals[i - len(self.est1.terminals)][0],self.est2.terminals[i - len(self.est1.terminals)][1],self.z1])
-        print(f'terminals : {self.terminals}')
+
         
+        dict_index_coordinates_terminals = {t_index[i]:self.terminals[i] for i in range(self.num_t)}
+        self.dict_index_coordinates_terminals = dict_index_coordinates_terminals
+
+
+        dict_index_steiner = {value: index for index, value in enumerate(s_index)}
+        self.dict_index_steiner = dict_index_steiner
+        print(f'index steiner : {dict_index_steiner}')
+
+
+        print(f'index connections : {self.connection_index}')
+        print(f'dict index coordinates terminals : {dict_index_coordinates_terminals}')        
+
+
     def solve_minimize(self):
+              
 
 
-        # def objective(vars):
-        #     self.s = np.zeros(3*self.num_s)
-        #     # print(f'vars : {vars}')
-        #     for i in range(self.num_s):
-        #         self.s[3*i:3*i+3] = vars[3*i:3*i+3]
-            
-        #     # print(f's : {self.s}')
-            
-        #     objt = 0
-        #     # print(f'terminals : {self.est1.terminals}')
-        #     for i in range(len(self.connection_index)):
-        #         # print(f'connection_index : {self.connection_index[i]}')
-                
-        #         if self.connection_index[i][0] > (self.num_t - 1):
-        #             a = self.s[int(self.connection_index[i][0])]
-        #             # print(f'aa : {a}')
-        #         elif self.connection_index[i][0] < len(self.est1.terminals):
-        #             a = np.array([self.est1.terminals[int(self.connection_index[i][0])][0],self.est1.terminals[int(self.connection_index[i][0])][1],self.z0])
-        #             # print(f'ab : {a}')   
-        #         else:
-        #             a = np.array([self.est1.terminals[int(self.connection_index[i][0] - len(self.est1.terminals))][0],self.est2.terminals[int(self.connection_index[i][0] - len(self.est1.terminals))][1],self.z1]) 
-        #             # print(f'ac : {a}')  
+        initial_guess = np.repeat(0, 3 *  self.num_s)
 
-        #         if self.connection_index[i][1] > (self.num_t - 1):
-        #             b = self.s[int(self.connection_index[i][1])]
-        #             # print(f'ba : {b}')
-        #         elif self.connection_index[i][1] < len(self.est1.terminals):
-        #             b = np.array([self.est1.terminals[int(self.connection_index[i][1])][0],self.est1.terminals[int(self.connection_index[i][1])][1],self.z0])
-        #             # print(f'bb : {b}')
-        #         else:
-        #             b = np.array([self.est1.terminals[int(self.connection_index[i][1] - len(self.est1.terminals))][0],self.est2.terminals[int(self.connection_index[i][1] - len(self.est1.terminals))][1],self.z1])
-        #             # print(f'bc : {b}')
-        #         objt += np.linalg.norm(a - b)
-        #         # print(f'objt : {objt}')
-        #     # print(objt)
-        #     return objt
+        print(f'num s : {self.num_s}')
 
-
-                        
-
-
-        # initial_guess = np.repeat(0, 3 *  self.num_s)
-
-        # print(f'initial_guess : {initial_guess}')
-
-        # result = minimize(objective, initial_guess,method="Powell")
-        # print(result)
-        # # for i in range(self.num_s):
-        # #     print(f's{i} : {result.x[3*i:3*i+3]}')
-        # return result.x
-        # Define the points a, b, and c
-        # t1 = self.terminals[0]
-        # t2 = self.terminals[1]
-        # t3 = self.terminals[2]
-        # t4 = self.terminals[3]
-        # t5 = self.terminals[4]
-        # t6 = self.terminals[5]
-        # t7 = self.terminals[6]
-        # t8 = self.terminals[7]
-
-        t1 = np.array([0, 0, 0])
-        t2 = np.array([0, 0, 1])
-        t3 = np.array([0, 1, 0])
-        t4 = np.array([0, 1, 1])
-        t5 = np.array([1, 0, 0])
-        t6 = np.array([1, 0, 1])
-        t7 = np.array([1, 1, 0])
-        t8 = np.array([1, 1, 1])
-
-
-        # c = np.array([1, 1, 0]) # Given but not used in the function
 
         # Define the objective function
-        def objective(vars):
-            s1, s2, s3, s4, s5, s6 = vars[:3], vars[3:6],vars[6:9],vars[9:12],vars[12:15],vars[15:18]  # Split the variables into x and y
-            return np.linalg.norm(t1 - s1) + np.linalg.norm(t2 - s1) + np.linalg.norm(s2 - t3)+ np.linalg.norm(s2 - t4)+np.linalg.norm(s5- s1)+np.linalg.norm(s2 - s5)+np.linalg.norm(s5 - s6)+np.linalg.norm(s3 - s6)+np.linalg.norm(s4 -s6)+np.linalg.norm(s3 - t5)+np.linalg.norm(s3 - t6)+np.linalg.norm(s4 - t7)+np.linalg.norm(s4 - t8)
 
-        # Initial guess for x and y
-        initial_guess = np.repeat(0,18)  # Assuming both x and y start from the origin
+        def objective_auto(vars):
+            index_connections = self.connection_index
+            dict_index_coordinates_terminals = self.dict_index_coordinates_terminals
+            obj = 0
+            for i in range(len(index_connections)):
+                if index_connections[i][0] in dict_index_coordinates_terminals.keys():
+                    a = dict_index_coordinates_terminals[index_connections[i][0]]
+                else:
+                    a = vars[3 * self.dict_index_steiner[index_connections[i][0]]:3 * self.dict_index_steiner[index_connections[i][0]] + 3]
 
+                if index_connections[i][1] in dict_index_coordinates_terminals.keys():
+                    b = dict_index_coordinates_terminals[index_connections[i][1]]
+                else:
+                    b = vars[3 * self.dict_index_steiner[index_connections[i][1]]:3 * self.dict_index_steiner[index_connections[i][1]] + 3]
+
+                obj += np.linalg.norm(a - b)
+            return obj
+
+
+    
         # Perform the optimization
-        result = minimize(objective, initial_guess,method="Powell")
+        result = minimize(objective_auto, initial_guess,method="Powell")
 
-        # Extract the optimized values of x and y
-        # x_opt, y_opt = result.x[:3], result.x[3:]
-        s1, s2, s3, s4, s5, s6 = result.x[:3], result.x[3:6],result.x[6:9],result.x[9:12],result.x[12:15],result.x[15:18]  # Split the variables into x and y
-
-        # print("Optimized x:", x_opt)
-        # print("Optimized y:", y_opt)
 
         print(result)
 
